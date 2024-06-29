bcaf9c43be5ea0d6c1ace82f832b388df20a6374
Luiz Monferrari <lmonferrrai@outlook.com>
2024-06-29 14:02:37 -0300

    salvando sempre o ultimo em produção

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     basedir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.dirname(basedir)
 
+    production_dir = "production"
     artifacts_dir = "artifacts"
     model_subdir = "model"
     data_dir = "data"
@@ -16,9 +17,10 @@ if __name__ == "__main__":
 
     execution_id = str(len(os.listdir(os.path.join(parent_dir, artifacts_dir))) + 1)
     model_path = os.path.join(parent_dir, artifacts_dir, execution_id, model_subdir)
+    production_path = os.path.join(parent_dir, production_dir)
 
-    if not os.path.exists(model_path):
-        os.makedirs(model_path)
+    os.makedirs(model_path, exist_ok=True)
+    os.makedirs(production_dir, exist_ok=True)
 
     create_data(os.path.join(data_path, dataset_name))
     df = pd.read_csv(os.path.join(data_path, dataset_name))
@@ -30,6 +32,7 @@ if __name__ == "__main__":
     model_instance.train(X, y)
 
     model_instance.save_model(model_path)
+    model_instance.save_model(production_path)
     model_instance.save_data(
         model_instance.X_train,
         model_instance.y_train,
